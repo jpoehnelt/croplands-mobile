e985f470471d2a26276187f1332ca834f9f3ffb0
Justin Poehnelt <justin.poehnelt@gmail.com>
2015-07-25 15:03:42 -0500

    added ability to get last fix accuracy

--- www/js/services/gps.js
+++ www/js/services/gps.js
@@ -1,12 +1,12 @@
 angular.module('croplandsApp.services')
-    .factory('GPS', ['$cordovaGeolocation', '$rootScope','Log', function ($cordovaGeolocation, $rootScope, Log) {
+    .factory('GPS', ['$cordovaGeolocation', '$rootScope','Log','$timeout', function ($cordovaGeolocation, $rootScope, Log, $timeout) {
         var geoWatch, // global for turning watch on/off
             positions = [], // array of positions
             watchOptions = {
                 frequency: 200, // how often to retrieve a position if possible
                 timeout: 10000, // when to throw an exception
                 enableHighAccuracy: true // use gps for higher resolution and when no network location is available
-            };
+            }, currentAccuracy = -1, currentAccuracyExpiration;
 
         /**
          * Creates a watch on geoLocation after close any open watches first.
@@ -23,6 +23,14 @@ angular.module('croplandsApp.services')
                     if(position && position.coords) {
                         positions.push(position);
                         $rootScope.$broadcast('GPS.on', position);
+                        currentAccuracy = position.coords.accuracy;
+                        if (currentAccuracyExpiration) {
+                            $timeout.cancel(currentAccuracyExpiration);
+                        }
+
+                        currentAccuracyExpiration = $timeout(function () {
+                            currentAccuracy = -1;
+                        }, 10000)
                     }
                 }
             );
@@ -61,13 +69,21 @@ angular.module('croplandsApp.services')
             return positions;
         }
 
+        /**
+         * Check if  there is a gps fix and return accuracy. No fix is -1
+         */
+        function getFix() {
+            return currentAccuracy;
+        }
+
         // Interface
         return {
             isOn: isOn,
             turnOn: turnOn,
             turnOff: turnOff,
             clearPositions: clearPositions,
-            getPositions: getPositions
+            getPositions: getPositions,
+            getFix: getFix
         }
     }
     ]);

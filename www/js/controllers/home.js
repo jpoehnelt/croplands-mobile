19b08e582029a5742a3079c25d71af6ff67ad344
Justin Poehnelt <justin.poehnelt@gmail.com>
2015-07-25 15:02:52 -0500

    cleanup unused imports

--- www/js/controllers/home.js
+++ www/js/controllers/home.js
@@ -1,13 +1,12 @@
 angular.module('croplandsApp.controllers')
-    .controller('HomeCtrl', ['$scope', '$stateParams', 'Location', '$http','Log','$q', 'Photos', function ($scope, $stateParams, Location, $http, Log, $q, Photos) {
+    .controller('HomeCtrl', ['$scope', '$stateParams', 'Location', '$http', 'Log', function ($scope, $stateParams, Location, $http, Log) {
 
         $scope.messages = Log.messages();
 
         $scope.canSync = Location.canSync;
         $scope.sync = function () {
-            Location.sync().then(function(){
+            Location.sync().then(function () {
                 Log.info("Sync complete");
             });
-
         };
     }]);

e85079d7e3790303fbce0564e9064ebb52de30e4
Justin Poehnelt <justin.poehnelt@gmail.com>
2015-09-26 12:13:47 -0700

    setup for testing with karma

--- karma.conf.js
+++ karma.conf.js
@@ -60,9 +60,21 @@ module.exports = function (config) {
         // available browser launchers: https://npmjs.org/browse/keyword/karma-launcher
         browsers: ['Chrome'],
 
+        customLaunchers: {
+            Chrome_travis_ci: {
+                base: 'Chrome',
+                flags: ['--no-sandbox']
+            }
+        },
+
 
         // Continuous Integration mode
         // if true, Karma captures browsers, runs the tests and exits
         singleRun: true
-    })
+    });
+
+    if (process.env.TRAVIS) {
+        configuration.browsers = ['Chrome_travis_ci'];
+
+    }
 };

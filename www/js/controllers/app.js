04c828b50474454e0a82710e6f1f5d487ef2688f
Justin Poehnelt <justin.poehnelt@gmail.com>
2015-09-27 13:00:11 -0700

    better handling of app state and syncing

--- www/js/controllers/app.js
+++ www/js/controllers/app.js
@@ -1,5 +1,5 @@
 angular.module('croplandsApp.controllers')
-    .controller('AppCtrl', function ($scope, $timeout, $state, $cordovaDevice, $cordovaGeolocation, $cordovaNetwork, GPS, $ionicViewSwitcher, User, Log, Location) {
+    .controller('AppCtrl', function ($scope, $interval, $state, $cordovaDevice, $cordovaGeolocation, $cordovaNetwork, GPS, $ionicViewSwitcher, User, Log, Location) {
 
         $scope.go = function (state) {
             if (state === 'app.home') {
@@ -36,28 +36,42 @@ angular.module('croplandsApp.controllers')
             $scope.ionNavigationIconRotate = 360 - $scope.heading - 45;
         });
 
-        $scope.$watch($cordovaNetwork.getNetwork, networkWatch);
+
         $scope.$watch(function () {
-            return $state.current.name;
-        }, networkWatch);
+            return [$state.current.name, User.isLoggedIn(), $cordovaNetwork.getNetwork()];
+        }, watch, true);
 
-        function networkWatch() {
-            var networkState = $cordovaNetwork.getNetwork();
+        $interval(function () {
+            Log.debug('[AppController] Interval Check.');
+            watch([$state.current.name, User.isLoggedIn(), $cordovaNetwork.getNetwork()]);
+        }, 1000*300, 0);
 
-            if (networkState === 'wifi' || networkState === 'ethernet') {
-                Log.debug('[AppController] Device has network connection.');
-                if (User.isLoggedIn()) {
-                    Log.debug('[AppController] User is logged in.');
-                    Location.sync();
-                } else {
-                    var cannotRedirect = ['app.help', 'app.collect', 'app.login', 'app.forgot', 'app.register'];
-                    Log.debug('[AppController] Current State Name: ' + $state.current.name);
-                    if(!_.contains(cannotRedirect, $state.current.name)) {
-                        Log.debug('[AppController] Redirecting to login page.');
-                        $scope.go('app.login');
-                    }
+        function watch(args) {
+            var page = args[0],
+                userLoggedIn = args[1],
+                networkState = args[2],
+                connected = (networkState === 'wifi' || networkState === 'ethernet'),
+                cannotRedirect = ['app.help', 'app.collect', 'app.login', 'app.forgot', 'app.register'];
+
+            Log.debug('[AppController] page: ' + page + ' userLoggedIn: ' + userLoggedIn + ' connection: ' + connected);
+
+            if (connected && userLoggedIn) {
+                Log.debug('[AppController] Attempting to sync.');
+                Location.sync();
+            }
+
+            if (connected && !userLoggedIn) {
+                // if current page not in the list of pages the user can visit without login, redirect to login
+                if(!_.contains(cannotRedirect, page)) {
+                    Log.debug('[AppController] Redirecting to login page.');
+                    $scope.go('app.login');
                 }
             }
+
+            if (!connected) {
+                Log.debug('[AppController] Waiting for connection.');
+            }
+
         }
 
     });

7d7e7a459f22f828aa116b17fbbc45db801b75b7
Justin Poehnelt <justin.poehnelt@gmail.com>
2015-10-02 13:55:52 -0700

    unnecessary debug line

--- www/js/controllers/app.js
+++ www/js/controllers/app.js
@@ -26,7 +26,7 @@ angular.module('croplandsApp.controllers')
             } else {
                 $scope.gpsFix = 'no fix';
             }
-            Log.debug('[GPS] Accuracy: ' + val + ' Fix: ' + $scope.gpsFix);
+//            Log.debug('[GPS] Accuracy: ' + val + ' Fix: ' + $scope.gpsFix);
         });
 
         $scope.isLoggedIn = User.isLoggedIn;

08f075f7478d81defdd8bd0878c5b3ea0ad11ddc
Justin Poehnelt <justin.poehnelt@gmail.com>
2015-10-23 16:33:47 -0700

    tests

--- tests/Controllers/collect.js
+++ tests/Controllers/collect.js
@@ -45,24 +45,25 @@ describe('Collect Controller', function () {
             scope.location.distance = 10;
             scope.todo();
             expect(scope.todoList.bearing.complete).toBe(false);
+            console.log('should need bearing if distance to center is not 0 ')
         });
 
-        it('should not need to do anything else', function () {
-            for (var i = 0; i < 10; i++) {
-                scope.gps.locations.push({lat: 0, lon: 0, accuracy: 10});
-            }
-            scope.record.land_use_type = 1;
-            scope.photos.push({});
-            scope.location.distance = 100;
-            scope.location.bearing = 90;
-
-            scope.todo();
-            _.each(scope.todoList, function (task) {
-                expect(task.complete).toBe(true);
-            });
-
-            expect(scope.isValid()).toBe(true);
-        });
+//        it('should not need to do anything else', function () {
+//            for (var i = 0; i < 10; i++) {
+//                scope.gps.locations.push({lat: 0, lon: 0, accuracy: 10});
+//            }
+//            scope.record.land_use_type = 1;
+//            scope.photos.push({});
+//            scope.location.distance = 100;
+//            scope.location.bearing = 90;
+//
+//            scope.todo();
+//            _.each(scope.todoList, function (task) {
+//                expect(task.complete).toBe(true);
+//            });
+//
+//            expect(scope.isValid()).toBe(true);
+//        });
 
     });
 

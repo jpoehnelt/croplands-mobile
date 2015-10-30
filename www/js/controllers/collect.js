68a3199a7a233fbda91046126e97fb51440db9e6
Justin Poehnelt <justin.poehnelt@gmail.com>
2015-10-29 19:29:58 -0700

    unnecessary init since no longer cached

--- www/js/controllers/collect.js
+++ www/js/controllers/collect.js
@@ -67,7 +67,6 @@ angular.module('croplandsApp.controllers')
         function cleanUp() {
             $scope.gps.on = false;
             $scope.gpsClear();
-            init();
             Log.debug('[CollectCtr] Cleanup complete')
         }
 

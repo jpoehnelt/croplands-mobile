6b2982e98555b80b14c0491a2726eb41a9c78c97
Justin Poehnelt <justin.poehnelt@gmail.com>
2015-11-03 14:23:33 -0700

    more tests on controller photo and save

--- tests/Controllers/collect.js
+++ tests/Controllers/collect.js
@@ -6,7 +6,7 @@ describe('Collect Controller', function () {
     beforeEach(module('templates'));
 
     var ctrl,
-        scope, CompassMock, rootScope;
+        scope, CompassMock, rootScope, cordovaCamera, $q, state;
 
     beforeEach(function () {
         var heading = {
@@ -24,12 +24,17 @@ describe('Collect Controller', function () {
         }
     });
 
-    beforeEach(inject(function ($rootScope, $controller) {
+    beforeEach(inject(function ($rootScope, $controller, $cordovaCamera, _$q_, $state) {
         scope = $rootScope.$new();
         rootScope = $rootScope;
+        cordovaCamera = $cordovaCamera;
+        state = $state;
+        $q = _$q_;
         ctrl = $controller('CollectCtrl', {
             $scope: scope,
-            Compass: CompassMock
+            Compass: CompassMock,
+            $cordovaCamera: cordovaCamera,
+            $state: state
         });
     }));
 
@@ -120,14 +125,20 @@ describe('Collect Controller', function () {
             scope.captureHeading();
             expect(scope.location.bearing).toBe(CompassMock.getHeading().magneticHeading)
         });
+
+        it('should not set if no result', function () {
+            CompassMock.setHeading(undefined);
+            scope.captureHeading();
+            expect(scope.location.bearing).toBe(null);
+        });
     });
 
     describe('gps positions', function () {
         var position = {
             coords: {
                 accuracy: 1,
-                lat: 0,
-                lon: 10
+                latitude: 0,
+                longitude: 10
             },
             timestamp: Date.now()
         };
@@ -144,6 +155,96 @@ describe('Collect Controller', function () {
             rootScope.$broadcast('GPS.on', position);
             expect(scope.gps.locations.length).toBe(0);
         });
+
+        it('should be cleared with $scope.gpsClear', function () {
+            scope.gps.on = true;
+            rootScope.$broadcast('GPS.on', position);
+            expect(scope.gps.locations.length).toBe(1);
+            scope.gpsClear();
+            expect(scope.gps.locations.length).toBe(0);
+        });
+
+        it('should have the correct mean', function () {
+            expect(scope.location.lat).toBe(null);
+            expect(scope.location.lon).toBe(null);
+            scope.gps.on = true;
+            rootScope.$broadcast('GPS.on', position);
+            scope.$digest();
+            expect(scope.location.lat).toBe(position.coords.latitude);
+            expect(scope.location.lon).toBe(position.coords.longitude);
+
+            var position2 = {
+                coords: {
+                    accuracy: 1,
+                    latitude: 10,
+                    longitude: 0
+                },
+                timestamp: Date.now()
+            };
+            rootScope.$broadcast('GPS.on', position2);
+            scope.$digest();
+            expect(scope.gps.locations.length).toBe(2);
+            expect(scope.location.lat).toBe((position.coords.latitude + position2.coords.latitude) / 2);
+            expect(scope.location.lon).toBe((position.coords.longitude + position2.coords.longitude) / 2);
+
+        })
+    });
+
+    describe('photos', function () {
+        var uri = 'asdfkadsfjka.jpg';
+
+        beforeEach(function () {
+            spyOn(cordovaCamera, "getPicture").and.returnValue($q.when(uri));
+            window.Camera = {
+                DestinationType: {
+                    NATIVE_URI: 'asdf'
+                },
+                PictureSourceType: {
+                    CAMERA: 'asdf'
+                }
+            }
+        });
+
+        it('should be capture with $scope.takePhoto', function () {
+            scope.takePhoto();
+            scope.$digest();
+            expect(scope.photos.length).toBe(1);
+            expect(scope.photos[0]).toBe(uri);
+        });
+
+        it('should be able to remove photo', function () {
+            scope.takePhoto();
+            scope.$digest();
+            scope.removePhoto(0);
+            scope.$digest();
+            expect(scope.photos.length).toBe(0);
+        });
+
+    });
+
+    describe('saving', function () {
+
+        beforeEach(function () {
+            spyOn(state, "go").and.returnValue();
+
+            for (var i = 0; i < 10; i++) {
+                scope.gps.locations.push({lat: 0, lon: 0, accuracy: 10});
+            }
+            scope.record.land_use_type = 1;
+            scope.record.crop_primary = 1;
+            scope.photos.push('asdfasdf.jpg');
+            scope.location.distance = 100;
+            scope.location.bearing = 90;
+
+            scope.gps.on = true;
+        });
+
+        it('should set to not capture gps points', function () {
+            scope.save();
+            expect(scope.gps.on).toBe(false);
+        });
+
+        // TODO More Tests
     });
 
 
